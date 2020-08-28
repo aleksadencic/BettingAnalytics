b9bc53a7f685e508db3155dfff0234dbc935bf9e
aleksa.dencic <alekdencic@hotmail.com>
2020-08-28 14:29:48 +0200

    Financial analytics effects fix

--- betting-analytics-ui/src/app/financial-analytics/financial-analytics.effects.ts
+++ betting-analytics-ui/src/app/financial-analytics/financial-analytics.effects.ts
@@ -1,15 +1,31 @@
-import { Actions, createEffect } from '@ngrx/effects';
-import { Observable } from 'rxjs';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { Observable, of } from 'rxjs';
 import { Action } from '@ngrx/store';
 import { Injectable } from '@angular/core';
+import * as FinancialsActions from './financial-analytics.actions';
+import { FinancialAnalyticsService } from '../services/financial-analytics.service';
+import { map, switchMap, catchError } from 'rxjs/operators';
 
 @Injectable()
 export class FinancialAnalyticsEffect {
     constructor(
-        private actions$: Actions
+        private actions$: Actions,
+        private financialsService: FinancialAnalyticsService
     ){}
 
     // addMember$: Observable<Action> = createEffect(() => {
 
     // })
+
+    // getFinancials$: Observable<Action> = createEffect(() =>
+    //   this.actions$.pipe(
+    //     ofType(FinancialsActions.setFinancialsData),
+    //     switchMap(() => {
+    //       return this.financialsService.getFinancials().pipe(
+    //         map(results => FinancialsActions.setFinancialsData({ data: results })),
+    //         catchError(error => of(FinancialsActions.setFinancialsDataError(error)))
+    //       );
+    //     }),
+    //   ),
+    // );
 }
